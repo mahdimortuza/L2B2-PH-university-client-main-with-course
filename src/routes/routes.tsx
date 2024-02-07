020bb2bffe97b516b3ef43ca36a4bff58b563b08
Mahdi Mortuza <rah@localhost.localdomain>
2024-02-07 14:26:20 +0600

    routes added

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,45 +1,33 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
-import About from "../pages/About";
-import Contact from "../pages/Contact";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
-import AdminDashboard from "../pages/admin/AdminDashboard";
-import CreateStudent from "../pages/admin/CreateStudent";
+import { adminPaths } from "./admin.routes";
+import { facultyPaths } from "./faculty.routes";
+import { studentPaths } from "./student.routes";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-    children: [
-      {
-        path: "about",
-        element: <About />,
-      },
-      {
-        path: "contact",
-        element: <Contact />,
-      },
-    ],
   },
 
   {
     path: "/admin",
     element: <App />,
-    children: [
-      {
-        index: true,
-        element: <AdminDashboard />,
-      },
-      {
-        path: "dashboard",
-        element: <AdminDashboard />,
-      },
-      {
-        path: "create-student",
-        element: <CreateStudent />,
-      },
-    ],
+    children: adminPaths,
+  },
+
+  {
+    path: "/faculty",
+    element: <App />,
+    children: facultyPaths,
+  },
+
+  {
+    path: "/student",
+    element: <App />,
+    children: studentPaths,
   },
 
   {
