5e18837f8bba184ebca8042688ccf0e9956128c9
Mahdi Mortuza <rah@localhost.localdomain>
2024-02-07 12:41:25 +0600

    coponent pagesadded

--- src/components/layout/AdminLayout.tsx
+++ src/components/layout/AdminLayout.tsx
@@ -1,7 +1,10 @@
+import { Outlet } from "react-router-dom";
+
 const AdminLayout = () => {
   return (
     <div>
-      <h1>admin layout.</h1>
+      <h1>this is navbar</h1>
+      <Outlet />
     </div>
   );
 };
--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,4 +1,5 @@
 import { Layout, Menu, MenuProps } from "antd";
+import { Outlet } from "react-router-dom";
 const { Header, Content, Footer, Sider } = Layout;
 
 const items: MenuProps["items"] = [
@@ -67,7 +68,7 @@ const MainLayout = () => {
               minHeight: 360,
             }}
           >
-            <h1>main content should go here.</h1>
+            <Outlet />
           </div>
         </Content>
         <Footer style={{ textAlign: "center" }}>
