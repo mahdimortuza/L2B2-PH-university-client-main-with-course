f899fdc6508f7d4f7cb809df582f25438ac42263
Mahdi Mortuza <rah@localhost.localdomain>
2024-02-12 19:50:40 +0600

    routes generator added

--- src/routes/admin.routes.tsx
+++ src/routes/admin.routes.tsx
@@ -16,7 +16,7 @@ type TSidebarItem = {
   children?: TSidebarItem[];
 };
 
-const adminPaths = [
+export const adminPaths = [
   {
     name: "Admin Dashboard",
     path: "dashboard",
@@ -74,23 +74,23 @@ export const adminSidebarItems = adminPaths.reduce(
 
 //* programatical way
 
-export const adminRoutes = adminPaths.reduce((acc: TRoute[], item) => {
-  if (item.path && item.element) {
-    acc.push({
-      path: item.path,
-      element: item.element,
-    });
-  }
-  if (item.children) {
-    item.children.forEach((child) => {
-      acc.push({
-        path: child.path,
-        element: child.element,
-      });
-    });
-  }
-  return acc;
-}, []);
+// export const adminRoutes = adminPaths.reduce((acc: TRoute[], item) => {
+//   if (item.path && item.element) {
+//     acc.push({
+//       path: item.path,
+//       element: item.element,
+//     });
+//   }
+//   if (item.children) {
+//     item.children.forEach((child) => {
+//       acc.push({
+//         path: child.path,
+//         element: child.element,
+//       });
+//     });
+//   }
+//   return acc;
+// }, []);
 
 //! hard coded way
 
