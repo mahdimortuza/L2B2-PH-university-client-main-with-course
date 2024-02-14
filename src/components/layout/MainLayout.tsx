09384a9a7a71c4f2985aedb0219680ad91331310
Mahdi Mortuza <mahdi.webx@gmail.com>
2024-02-14 09:38:30 +0600

    sonner added

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,7 +1,9 @@
-import { Layout } from "antd";
+import { Button, Layout } from "antd";
 import { Outlet } from "react-router-dom";
+import { logout } from "../../redux/features/auth/authSlice";
+import { useAppDispatch } from "../../redux/hooks";
 import Sidebar from "./Sidebar";
-const { Header, Content, Footer } = Layout;
+const { Header, Content } = Layout;
 
 // const items: MenuProps["items"] = [
 //   {
@@ -30,11 +32,19 @@ const { Header, Content, Footer } = Layout;
 // ];
 
 const MainLayout = () => {
+  const dispatch = useAppDispatch();
+  const handleLogout = () => {
+    dispatch(logout());
+  };
+
   return (
     <Layout style={{ height: "100vh" }}>
+      <Sidebar />
       <Layout>
-        <Sidebar />
-        <Header style={{ padding: 0 }} />
+        <Header>
+          <Button onClick={handleLogout}>Logout</Button>
+        </Header>
+
         <Content style={{ margin: "24px 16px 0" }}>
           <div
             style={{
