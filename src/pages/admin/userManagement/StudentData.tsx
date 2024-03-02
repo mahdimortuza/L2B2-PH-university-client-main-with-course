70855ad599b4e782deefd30c2458bc306971df5b
mahdimortuza <mahdi.webx@gmail.com>
2024-03-02 18:17:37 +0600

    student done

--- src/pages/admin/userManagement/StudentData.tsx
+++ src/pages/admin/userManagement/StudentData.tsx
@@ -7,16 +7,19 @@ import {
   TableProps,
 } from "antd";
 import { useState } from "react";
+import { Link } from "react-router-dom";
 import { useGetAllStudentsQuery } from "../../../redux/features/admin/userManagement.api";
 import { TQueryParams, TStudent } from "../../../types";
 
-export type TTableData = Pick<TStudent, "fullName" | "id">;
+export type TTableData = Pick<
+  TStudent,
+  "fullName" | "id" | "email" | "contactNo"
+>;
 
 const StudentData = () => {
   const [params, setParams] = useState<TQueryParams[]>([]);
-  const [page, setPage] = useState(2);
+  const [page, setPage] = useState(1);
   const { data: studentData, isFetching } = useGetAllStudentsQuery([
-    { name: "limit", value: 4 },
     { name: "page", value: page },
     { name: "sort", value: "id" },
     ...params,
@@ -24,11 +27,15 @@ const StudentData = () => {
 
   const metaData = studentData?.meta;
 
-  const tableData = studentData?.data?.map(({ _id, fullName, id }) => ({
-    key: _id,
-    fullName,
-    id,
-  }));
+  const tableData = studentData?.data?.map(
+    ({ _id, fullName, id, email, contactNo }) => ({
+      key: _id,
+      fullName,
+      id,
+      email,
+      contactNo,
+    })
+  );
 
   const columns: TableColumnsType<TTableData> = [
     {
@@ -42,13 +49,27 @@ const StudentData = () => {
       key: "id",
       dataIndex: "id",
     },
+
+    {
+      title: "Email",
+      key: "email",
+      dataIndex: "email",
+    },
+    {
+      title: "Contact No.",
+      key: "contactNo",
+      dataIndex: "contactNo",
+    },
     {
       title: "Action",
       key: "x",
-      render: () => {
+      render: (item) => {
+        console.log(item);
         return (
           <Space>
-            <Button>Details</Button>
+            <Link to={`/admin/student-data/${item.key}`}>
+              <Button>Details</Button>
+            </Link>
             <Button>Update</Button>
             <Button>Block</Button>
           </Space>
---
+++ src/pages/admin/userManagement/StudentDetails.tsx
@@ -0,0 +1,9 @@
+import { useParams } from "react-router-dom";
+
+const StudentDetails = () => {
+  const { studentId } = useParams();
+
+  return <div>Student Details of {`${studentId}`}</div>;
+};
+
+export default StudentDetails;
